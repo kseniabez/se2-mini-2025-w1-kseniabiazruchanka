6431ade28874adb9fb24bc2d0693330d44e38425
Ksenia Biazruchanka <kseniabezruchenko@gmail.com>
2025-03-06 15:43:11 +0100

    change to pytest

--- tests.py
+++ tests.py
@@ -1,115 +1,111 @@
-import unittest
+import pytest
 from calculator import calculate_string
 
-class TestCalculateString(unittest.TestCase):
-    def test_empty_string_returns_zero(self):
-        # Given
-        input_string = ""
-        expected_result = 0
+def test_empty_string_returns_zero():
+    # Given
+    input_string = ""
+    expected_result = 0
 
-        # When
-        result = calculate_string(input_string)
+    # When
+    result = calculate_string(input_string)
 
-        # Then
-        self.assertEqual(result, expected_result)
+    # Then
+    assert result == expected_result
 
-    def test_single_number_returns_value(self):
-        # Given
-        input_string = "1"
-        expected_result = 1
+def test_single_number_returns_value():
+    # Given
+    input_string = "1"
+    expected_result = 1
 
-        # When
-        result = calculate_string(input_string)
+    # When
+    result = calculate_string(input_string)
 
-        # Then
-        self.assertEqual(result, expected_result)
+    # Then
+    assert result == expected_result
 
-    def test_two_numbers_comma_delimited_returns_sum(self):
-        # Given
-        input_string = "22,8"
-        expected_result = 30
+def test_two_numbers_comma_delimited_returns_sum():
+    # Given
+    input_string = "22,8"
+    expected_result = 30
 
-        # When
-        result = calculate_string(input_string)
+    # When
+    result = calculate_string(input_string)
 
-        # Then
-        self.assertEqual(result, expected_result)
+    # Then
+    assert result == expected_result
 
-    def test_two_numbers_newline_delimited_returns_sum(self):
-        # Given
-        input_string = "22\n8"
-        expected_result = 30
+def test_two_numbers_newline_delimited_returns_sum():
+    # Given
+    input_string = "22\n8"
+    expected_result = 30
 
-        # When
-        result = calculate_string(input_string)
+    # When
+    result = calculate_string(input_string)
 
-        # Then
-        self.assertEqual(result, expected_result)
+    # Then
+    assert result == expected_result
 
-    def test_three_numbers_delimited_returns_sum(self):
-        # Given
-        input_string = "21\n8,1"
-        expected_result = 30
+def test_three_numbers_delimited_returns_sum():
+    # Given
+    input_string = "21\n8,1"
+    expected_result = 30
 
-        # When
-        result = calculate_string(input_string)
+    # When
+    result = calculate_string(input_string)
 
-        # Then
-        self.assertEqual(result, expected_result)
+    # Then
+    assert result == expected_result
 
-    def test_negative_numbers_throw_exception(self):
-        # Given
-        input_string = "1,-2,3"
+def test_negative_numbers_throw_exception():
+    # Given
+    input_string = "1,-2,3"
 
-        # When/Then:
-        with self.assertRaises(ValueError) as context:
-            calculate_string(input_string)
+    # When/Then
+    with pytest.raises(ValueError) as context:
+        calculate_string(input_string)
 
-        self.assertIn("Negative numbers are not allowed.", str(context.exception))
+    assert "Negative numbers are not allowed." in str(context.value)
 
-    def test_numbers_greater_than_1000_are_ignored(self):
-        # Given
-        input_string = "2,1001,3"
-        expected_result = 5
+def test_numbers_greater_than_1000_are_ignored():
+    # Given
+    input_string = "2,1001,3"
+    expected_result = 5
 
-        # When
-        result = calculate_string(input_string)
+    # When
+    result = calculate_string(input_string)
 
-        # Then
-        self.assertEqual(result, expected_result)
+    # Then
+    assert result == expected_result
 
-    def test_custom_delimiter(self):
-        # Given
-        input_string = "//#\n1#2#3"
-        expected_result = 6
+def test_custom_delimiter():
+    # Given
+    input_string = "//#\n1#2#3"
+    expected_result = 6
 
-        # When
-        result = calculate_string(input_string)
+    # When
+    result = calculate_string(input_string)
 
-        # Then
-        self.assertEqual(result, expected_result)
+    # Then
+    assert result == expected_result
 
-    def test_custom_multichar_delimiter(self):
-        # Given
-        input_string = "//[###]\n1###2###3"
-        expected_result = 6
+def test_custom_multichar_delimiter():
+    # Given
+    input_string = "//[###]\n1###2###3"
+    expected_result = 6
 
-        # When
-        result = calculate_string(input_string)
+    # When
+    result = calculate_string(input_string)
 
-        # Then
-        self.assertEqual(result, expected_result)
+    # Then
+    assert result == expected_result
 
-    def test_multiple_custom_delimiters(self):
-        # Given
-        input_string = "//[#][###]\n1#2###3"
-        expected_result = 6
+def test_multiple_custom_delimiters():
+    # Given
+    input_string = "//[#][###]\n1#2###3"
+    expected_result = 6
 
-        # When
-        result = calculate_string(input_string)
+    # When
+    result = calculate_string(input_string)
 
-        # Then
-        self.assertEqual(result, expected_result)
-
-if __name__ == "__main__":
-    unittest.main()
+    # Then
+    assert result == expected_result
